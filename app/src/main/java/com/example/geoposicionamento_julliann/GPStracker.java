6ce71e15a2708d22a14412361326114b327dfdd0
JulliannARamos <69683252+JulliannARamos@users.noreply.github.com>
2022-04-08 16:07:53 -0300

    RepositórioComentariosEduarda
    Tentativa de postar a conta. Comentarios de Eduarda

--- app/src/main/java/com/example/geoposicionamento_julliann/GPStracker.java
+++ app/src/main/java/com/example/geoposicionamento_julliann/GPStracker.java
@@ -12,40 +12,41 @@ import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.core.content.ContextCompat;
 
-public class GPStracker implements LocationListener {//criando o metodo da classe GPStracker
+public class GPStracker implements LocationListener {//foi criada a classe GPStracker
     Context context;
-    //Instanciando o metodo da classe GPStracker
+    //Instancia o metodo da classe GPStracker
     public GPStracker(Context c){
         context=c;
     }
-    //Criei o metodo getLocation que requer um retorno de um valor do tipo location
+    //foi criado um metodo getLocation que retonará um valor do tipo location
     public Location getLocation(){
-        //checando a permissão para poder acessar a localiação do dispositivo
+        // Será verificado a permissão de acesso a localização do dispositivo
         if (ContextCompat.checkSelfPermission(context, Manifest.permission.ACCESS_FINE_LOCATION)!= PackageManager.PERMISSION_GRANTED){
-            Toast.makeText(context, "Não foi permitido", Toast.LENGTH_SHORT).show();//caso não tenha permissão, irá aparecer a mensagem que não foi permitido
-            return  null;// retornaremos o valor nulo. Apos retornar o valor nulo, irá verificar se o serviço de gps está ativo.
+            Toast.makeText(context, "Não foi permitido", Toast.LENGTH_SHORT).show();//se nao foi permitido, aparecerá o texto "nao foi permitido"
+            return  null;//retorna valor nulo que irá confirmar se o gps está funcionando.
+
         }
         LocationManager lm= (LocationManager) context.getSystemService(Context.LOCATION_SERVICE);
         boolean isGPSEnabled = lm.isProviderEnabled(LocationManager.GPS_PROVIDER);
 
-        //Se o GPS estiver ativado, solicitação da atualização de localização do dispositivo
+        //se o GPS estiver ativado, será pedido a atualização de localização do dispositivo
         if (isGPSEnabled){
             lm.requestLocationUpdates(LocationManager.GPS_PROVIDER, 6000, 10, this);
             Location l = lm.getLastKnownLocation(LocationManager.GPS_PROVIDER);
             return l;
         }
         else {
-            //Irá aparecer na tela para habilitar o gps, caso nao esteja habilitado
+            //Irá aparecer um texto "por favor, habilitar o GPS!", se nao estiver habilitado
             Toast.makeText(context, "Por favor, habilitar o GPS!", Toast.LENGTH_LONG).show();
 
         }
-        return null;//retornando o valor nulo
+        return null;//retorna valor nulo
     }
     @Override
 
-    public  void  onProviderDisabled(@NonNull String provider){ }//sobrescrevendo metodo que registra o provedor
+    public  void  onProviderDisabled(@NonNull String provider){ }//transcreve metodo que registra o provedor
     @Override
-    public void onLocationChanged(@NonNull Location location){ }//sobrescrevendo metodo chamado quando a localização muda
+    public void onLocationChanged(@NonNull Location location){ }//transcreve metodo chamado quando a localização muda
     @Override
-    public void onStatusChanged(String provider, int status, Bundle extras){ }//sobrescrevendo o metodo quando há mudanças no status
+    public void onStatusChanged(String provider, int status, Bundle extras){ }//transcreve o metodo quando há mudanças no status
 }
--- app/src/main/java/com/example/geoposicionamento_julliann/MainActivity.java
+++ app/src/main/java/com/example/geoposicionamento_julliann/MainActivity.java
@@ -17,55 +17,56 @@ import android.widget.ImageView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    //declarou as variaveis
-    private ImageView imageViewFoto;
-    private Button btnGeo;
+    //declara variaveis
+    private ImageView imageViewFoto;//imagem
+    private Button btnGeo;//botão
 
-    @Override// sobrescrevendo metedo, "savedInstanceState" serve para recuperar um metodo
+    @Override//"savedInstanceState" serve para recuperar um metodo
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);//utilizado para setar o layout
+        setContentView(R.layout.activity_main);//seta o layout
 
-        //criando o botão localização
+        //botão de localização
         btnGeo = (Button) findViewById(R.id.btn_gps);
-        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 123);//pedindo para acessar a localização do celular
+        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 123);//pede permissão ao acesso da localização do celular
 
-        //chamando o metodo de pressionar o botão
+        //chama o metodo de pressionar o botão "SetOnClickListener"
         btnGeo.setOnClickListener(new View.OnClickListener() {
-            @Override//sobrescrevendo o metodo onClick
+            @Override//transcreve o metodo onClick
             public void onClick(View view) {
-                GPStracker g = new GPStracker(getApplication());//definindo que vou usar a classe GPStracker
-                Location l = g.getLocation();// pegando a localização do dispositivo com o getLocation
+                GPStracker g = new GPStracker(getApplication());//será usada a classe GPStracker
+                Location l = g.getLocation();// pega a localização do dispositivo com o getLocation
 
 
-                if (l != null) { //se a localização for diferente de nulo, irá pegar as variaveis de latitude e longitude, e vai mostrar na tela
+                if (l != null) { //se a localização for diferente de nulo, irá pegar as variaveis de latitude e longitude
                     double lat = l.getLatitude();
                     double lon = l.getLongitude();
                     Toast.makeText(getApplicationContext(), "LATITUDE:" + lat + "\n LONGITUDE:" + lon, Toast.LENGTH_LONG).show();
 
                 }
             }
-        });//checando se dei a permissão para o uso da camera, caso não tenha dado, irá pedir uma permissão
+        });//checando permissão ao uso da camera
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, 0);
         }
         //
-        imageViewFoto = (ImageView) findViewById(R.id.image_foto);// ira mostrar a imagem viewFot, e adicionar um clickListener
+        imageViewFoto = (ImageView) findViewById(R.id.image_foto);// adiciona um clickListener que serve pra cadastrar um objeto do tipo View(ImageViewFoto)
         findViewById(R.id.btn_pic).setOnClickListener(new View.OnClickListener() {
-            @Override//sobrescrevendo metodo, e chamando metodo tirarFoto
+            @Override//transcrevendo metodo, e chamando metodo tirarFoto
             public void onClick(View view) {
                 tirarFoto();
             }
         });
     }
-    private void  tirarFoto()//metodo tirar foto. Declarando a função tirarFoto e disponibilizando a camera para tirar a foto
+    private void  tirarFoto()//Declarando a função tirarFoto que irá disponibilizar a camera para tirar foto
     {
         Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
         startActivityForResult(intent, 1);
     }
     //@Override
-    protected void OnActivityResult(int requestCode, int resultCode, @Nullable Intent data){// sobrescrevendo o metodo para receber os resultados do metodo tirarFoto
-//Pegar a imagem que irá ser tirado pela camera e transformar em bitmap
+    protected void OnActivityResult(int requestCode, int resultCode, @Nullable Intent data){// transcrevendo o metodo para receber os resultados do metodo tirarFoto
+
+        //a imagem tirada pela camera transforma em bitmap
         if (requestCode==1&&resultCode==RESULT_OK)
         {
             Bundle extras = data.getExtras();
